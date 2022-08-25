3543f7072f734235c47c9c4436c5d60e094eb638
Chibuike <lord.codefreak@gmail.com>
2022-08-25 20:48:59 +0100

    Modify index.html/index.js

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
-This is a JavaScript document.
+function ekele(){
+    let x = "Hello";
+    let y = "World";
+    let greetings = `${x} ${y}!`;
+    const aguNnekka = console.log(greetings);
+}
+
+ekele();
