109c8693fa7475cf6918bae73b158fc15effadee
tech-whiz <mbachibuikevictor@gmail.com>
2022-09-14 15:36:06 +0100

    Add mba.js, Modify .gitattributes

---
+++ mba.js
@@ -0,0 +1,10 @@
+#!/usr/bin/env node
+const tempConst = 273;
+let celcius = {integer_data_type}
+function kelToCelcius(celc) {
+	let kelTemp = tempConst + celcius;
+	console.log(kelTemp + " Degree kelvin", celc);
+	//return 809;
+}
+kelToCelcius(celcius);
+//console.log(kelToCelcius(celcius)* 2);
